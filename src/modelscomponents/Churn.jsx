9368a169d8fb7f3a68218541e0c0a6596a47e448
CristopherSC <cristophersc2815956@estudiantec.cr>
2023-04-17 03:14:46 -0600

    Update Churn.jsx

--- src/modelscomponents/Churn.jsx
+++ src/modelscomponents/Churn.jsx
@@ -1,9 +1,27 @@
 import { useEffect, useState } from "react";
 
 export const Churn = (props) => {
+  const [indiceCliente, setIndiceCliente] = useState(0);
+  const [centroBoton, setCentroBoton] = useState(0);
+
+  const siguienteCliente = () => {
+    setIndiceCliente(
+      indiceCliente === Object.keys(cliente).length - 1 ? 0 : indiceCliente + 1
+    );
+  };
+
+  const anteriorCliente = () => {
+    setIndiceCliente(
+      indiceCliente === 0 ? Object.keys(cliente).length - 1 : indiceCliente - 1
+    );
+  };
+
+  const mostrarCliente = (index) => {
+    setIndiceCliente(index);
+  };
 
   const [cliente, setcliente] = useState({
-    gender: "",
+    gender: "Si",
     SeniorCitizen: "",
     Partner: "",
     Dependents: "",
@@ -31,11 +49,27 @@ export const Churn = (props) => {
   };
 
   useEffect(() => {
-    saludar()
+    saludar();
+    setCentroBoton(Math.floor(Object.keys(cliente).length / 2));
   }, []);
 
+  const listaValores = Object.entries(cliente).map(([clave]) => (
+    <li key={clave} style={{ listStyleType: "none" }}>
+      {clave}
+    </li>
+  ));
+  const listaValores2 = Object.entries(cliente).map(([clave, valor]) => (
+    <li key={clave}>
+      {clave}: {valor}
+    </li>));
+
   return (
-  <>Churn</>
+    <div>
+      <ul>{listaValores2}</ul>
+      <button onClick={anteriorCliente}>Anterior</button>
+      <button>{listaValores[indiceCliente]}</button>
+      <button onClick={siguienteCliente}>Siguiente</button>
+    </div>
   );
 };
 
